01f0a4d2b0ef6d11d165bfd80215449b282310af
Gabriel Paganini <gabrielpaganinibarroso@hotmail.com>
2021-12-28 21:37:15 -0300

    criando os eventos dos botões

--- src/ui/ContactForm.java
+++ src/ui/ContactForm.java
@@ -1,6 +1,9 @@
 package ui;
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class ContactForm extends JFrame {
 
@@ -8,11 +11,35 @@ public class ContactForm extends JFrame {
     private JTextField textName;
     private JTextField textPhone;
     private JButton salvarButton;
-    private JButton calcelarButton;
+    private JButton cancelarButton;
 
     public ContactForm(){
         setContentPane(rootPanel);
         setVisible(true);
         setSize(500,250);
+
+        Dimension d = Toolkit.getDefaultToolkit().getScreenSize(); // Pega o tamanho da tela do monitor
+        setLocation(d.width/2 - getSize().width/2, d.height/2 - getSize().height/2); //Coloca a janela da agenda no meio da sua tela
+
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE); //Fala que quando a janela fechar, o programa para
+
+        setListeners();
+    }
+
+    private void setListeners(){
+        salvarButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+
+        cancelarButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                new MainForm();
+                dispose();// esconde o contact form
+            }
+        });
     }
 }
--- src/ui/MainForm.java
+++ src/ui/MainForm.java
@@ -2,6 +2,8 @@ package ui;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class MainForm extends JFrame {
     private JButton novoContatoButton;
@@ -17,6 +19,25 @@ public class MainForm extends JFrame {
         Dimension d = Toolkit.getDefaultToolkit().getScreenSize(); // Pega o tamanho da tela do monitor
         setLocation(d.width/2 - getSize().width/2, d.height/2 - getSize().height/2); //Coloca a janela da agenda no meio da sua tela
 
-        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE); //Fala que quando a janela fecha, o programa para
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE); //Fala que quando a janela fechar, o programa para
+
+        setListeners();// Metodo para atribuir eventos
+    }
+
+    private void setListeners(){
+        novoContatoButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                new ContactForm();
+                dispose();// esconde o main form
+            }
+        });
+
+        excluirContatoButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
     }
 }
