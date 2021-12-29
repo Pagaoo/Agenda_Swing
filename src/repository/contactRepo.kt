bfdeea1de2d200889b19977c93a342515d6fb2ee
Gabriel Paganini <gabrielpaganinibarroso@hotmail.com>
2021-12-29 18:32:22 -0300

    repositorio onde ficara a mutable list de contatos

--- src/repository/contactRepo.kt
+++ src/repository/contactRepo.kt
@@ -1,5 +1,22 @@
 package repository
 
+import entity.contactEntity
+
 class contactRepo {
 
+    companion object {
+        private val contactList = mutableListOf<contactEntity>()
+
+        fun saveContact(contact: contactEntity) {
+            contactList.add(contact)
+        }
+
+        fun deleteContact(contact: contactEntity) {
+
+        }
+
+        fun getContactList(): MutableList<contactEntity> {
+            return contactList
+        }
+    }
 }
